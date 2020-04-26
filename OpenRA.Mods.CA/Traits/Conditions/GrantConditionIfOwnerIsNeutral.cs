e2640c2cad5a2f4e5d7c87f1a453933b5a189734
Unknown <grayseer@aol.com>
2020-04-26 16:50:25 +0100

    + Add Jarmen Kell Logic to England Sniper
    + Add new BTR (Flak)
    + Adjust Aurora Speed/Damage reduction
    +Change MOAB sprite

---
+++ OpenRA.Mods.CA/Traits/Conditions/GrantConditionIfOwnerIsNeutral.cs
@@ -0,0 +1,73 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	[Desc("Grants a condition if the owner is the Neutral player.")]
+	public class GrantConditionIfOwnerIsNeutralInfo : ITraitInfo
+	{
+		[GrantedConditionReference]
+		[Desc("The condition to grant.")]
+		public readonly string Condition = null;
+
+		public object Create(ActorInitializer init) { return new GrantConditionIfOwnerIsNeutral(this); }
+	}
+
+	public class GrantConditionIfOwnerIsNeutral : INotifyCreated, INotifyOwnerChanged
+	{
+		readonly GrantConditionIfOwnerIsNeutralInfo info;
+		ConditionManager manager;
+
+		int token = ConditionManager.InvalidConditionToken;
+
+		public GrantConditionIfOwnerIsNeutral(GrantConditionIfOwnerIsNeutralInfo info)
+		{
+			this.info = info;
+		}
+
+		void INotifyCreated.Created(Actor self)
+		{
+			manager = self.Trait<ConditionManager>();
+
+			if (self.Owner.PlayerName == "Neutral")
+				GrantCondition(self, info.Condition);
+		}
+
+		void GrantCondition(Actor self, string condition)
+		{
+			if (manager == null)
+				return;
+
+			if (string.IsNullOrEmpty(condition))
+				return;
+
+			token = manager.GrantCondition(self, condition);
+		}
+
+		void RevokeCondition(Actor self)
+		{
+			if (manager == null)
+				return;
+
+			token = manager.RevokeCondition(self, token);
+		}
+
+		void INotifyOwnerChanged.OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
+		{
+			if (newOwner.PlayerName == "Neutral" && token == ConditionManager.InvalidConditionToken)
+				GrantCondition(self, info.Condition);
+			else if (newOwner.PlayerName != "Neutral" && token != ConditionManager.InvalidConditionToken)
+				RevokeCondition(self);
+		}
+	}
+}
---
+++ OpenRA.Mods.CA/Warheads/ChangeOwnerToNeutralWarhead.cs
@@ -0,0 +1,48 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.GameRules;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Warheads;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Warheads
+{
+	[Desc("Changes targets to neutral.")]
+	public class ChangeOwnerToNeutralWarhead : Warhead
+	{
+		[Desc("Faction to change to.")]
+		public readonly string Owner = "Neutral";
+
+		public readonly WDist Range = WDist.FromCells(1);
+
+		public override void DoImpact(Target target, WarheadArgs args)
+		{
+			var firedBy = args.SourceActor;
+			var actors = target.Type == TargetType.Actor ? new[] { target.Actor } :
+				firedBy.World.FindActorsInCircle(target.CenterPosition, Range);
+
+			foreach (var a in actors)
+			{
+				// Don't do anything on friendly fire
+				if (a.Owner == firedBy.Owner)
+					continue;
+
+				a.ChangeOwner(a.World.Players.First(p => p.InternalName == Owner)); // Permanent
+
+				// Stop shooting, you have new enemies
+				a.CancelActivity();
+			}
+		}
+	}
+}
