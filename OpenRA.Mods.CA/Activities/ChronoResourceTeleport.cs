5400203771555ac23598f6f1d67bb385c7cef85d
Unknown <grayseer@aol.com>
2020-05-16 10:04:35 +0100

    Add Chrono Harvester
    +Fix Vulcan prereq
    +Fix Jungle Crash
    +Fix TS Firestorm OST
    +Add RA2/YR Music compatibility

---
+++ OpenRA.Mods.CA/Activities/ChronoResourceTeleport.cs
@@ -0,0 +1,61 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Activities;
+using OpenRA.Mods.CA.Traits;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Effects;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Activities
+{
+	public class ChronoResourceTeleport : Activity
+	{
+		readonly CPos destination;
+		readonly ChronoResourceDeliveryInfo info;
+		readonly CPos harvestedField;
+
+		public ChronoResourceTeleport(CPos destination, ChronoResourceDeliveryInfo info, CPos harvestedField)
+		{
+			this.destination = destination;
+			this.info = info;
+			this.harvestedField = harvestedField;
+		}
+
+		public override bool Tick(Actor self)
+		{
+			var image = info.Image ?? self.Info.Name;
+
+			var sourcepos = self.CenterPosition;
+
+			if (info.WarpInSequence != null)
+				self.World.AddFrameEndTask(w => w.Add(new SpriteEffect(sourcepos, w, image, info.WarpInSequence, info.Palette)));
+
+			if (info.WarpInSound != null && (info.AudibleThroughFog || !self.World.FogObscures(sourcepos)))
+				Game.Sound.Play(SoundType.World, info.WarpInSound, self.CenterPosition, info.SoundVolume);
+
+			self.Trait<IPositionable>().SetPosition(self, destination);
+			self.Generation++;
+
+			var destinationpos = self.CenterPosition;
+
+			if (info.WarpOutSequence != null)
+				self.World.AddFrameEndTask(w => w.Add(new SpriteEffect(destinationpos, w, image, info.WarpOutSequence, info.Palette)));
+
+			if (info.WarpOutSound != null && (info.AudibleThroughFog || !self.World.FogObscures(sourcepos)))
+				Game.Sound.Play(SoundType.World, info.WarpOutSound, self.CenterPosition, info.SoundVolume);
+
+			self.QueueActivity(new FindAndDeliverResources(self, harvestedField));
+
+			return true;
+		}
+	}
+}
---
+++ OpenRA.Mods.CA/Traits/ChronoResourceDelivery.cs
@@ -0,0 +1,126 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.CA.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	[Desc("When returning to a refinery to deliver resources, this actor will teleport if possible.")]
+	public class ChronoResourceDeliveryInfo : ConditionalTraitInfo, Requires<HarvesterInfo>
+	{
+		[Desc("The number of ticks between each check to see if we can teleport to the refinery.")]
+		public readonly int CheckTeleportDelay = 10;
+
+		[Desc("Image used for the teleport effects. Defaults to the actor's type.")]
+		public readonly string Image = null;
+
+		[Desc("Sequence used for the effect played where the harvester jumped from.")]
+		[SequenceReference("Image")]
+		public readonly string WarpInSequence = null;
+
+		[Desc("Sequence used for the effect played where the harvester jumped to.")]
+		[SequenceReference("Image")]
+		public readonly string WarpOutSequence = null;
+
+		[Desc("Palette to render the warp in/out sprites in.")]
+		[PaletteReference]
+		public readonly string Palette = "effect";
+
+		[Desc("Sound played where the harvester jumped from.")]
+		public readonly string WarpInSound = null;
+
+		[Desc("Sound where the harvester jumped to.")]
+		public readonly string WarpOutSound = null;
+
+		[Desc("Does the sound play under shroud or fog.")]
+		public readonly bool AudibleThroughFog = true;
+
+		[Desc("Volume the WarpInSound and WarpOutSound played at.")]
+		public readonly float SoundVolume = 1;
+
+		public override object Create(ActorInitializer init) { return new ChronoResourceDelivery(init.Self, this); }
+	}
+
+	public class ChronoResourceDelivery : ConditionalTrait<ChronoResourceDeliveryInfo>, INotifyHarvesterAction, ITick
+	{
+		CPos? destination = null;
+		CPos harvestedField;
+		int ticksTillCheck = 0;
+
+		public ChronoResourceDelivery(Actor self, ChronoResourceDeliveryInfo info)
+			: base(info) { }
+
+		void ITick.Tick(Actor self)
+		{
+			if (IsTraitDisabled || destination == null)
+				return;
+
+			if (ticksTillCheck <= 0)
+			{
+				ticksTillCheck = Info.CheckTeleportDelay;
+
+				TeleportIfPossible(self);
+			}
+			else
+				ticksTillCheck--;
+		}
+
+		public void MovingToResources(Actor self, CPos targetCell)
+		{
+			Reset();
+		}
+
+		public void MovingToRefinery(Actor self, Actor refineryActor)
+		{
+			var deliverypos = refineryActor.Location + refineryActor.Trait<IAcceptResources>().DeliveryOffset;
+
+			if (destination != null && destination.Value != deliverypos)
+				ticksTillCheck = 0;
+
+			harvestedField = self.World.Map.CellContaining(self.CenterPosition);
+
+			destination = deliverypos;
+		}
+
+		public void MovementCancelled(Actor self)
+		{
+			Reset();
+		}
+
+		public void Harvested(Actor self, ResourceType resource) { }
+		public void Docked() { }
+		public void Undocked() { }
+
+		void TeleportIfPossible(Actor self)
+		{
+			// We're already here; no need to interfere.
+			if (self.Location == destination.Value)
+			{
+				Reset();
+				return;
+			}
+
+			var pos = self.Trait<IPositionable>();
+			if (pos.CanEnterCell(destination.Value))
+			{
+				self.QueueActivity(false, new ChronoResourceTeleport(destination.Value, Info, harvestedField));
+				Reset();
+			}
+		}
+
+		void Reset()
+		{
+			ticksTillCheck = 0;
+			destination = null;
+		}
+	}
+}
