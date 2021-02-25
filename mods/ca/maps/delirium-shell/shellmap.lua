d550aaf0da7258f62f33541145b2efa06f6642a8
Inq <grayseer@aol.com>
2021-02-25 17:32:25 +1000

    0.72 Release

--- mods/ca/maps/delirium-shell/shellmap.lua
+++ mods/ca/maps/delirium-shell/shellmap.lua
@@ -11,6 +11,7 @@ NodLightUnitTypes = { "bike", "bike", "bggy", "rmbc", "rmbc", "n1c", "n1c", "n1c
 NodHeavyUnitTypes = { "mtnk", "mtnk", "ftnk", "ftnk", "wtnk", "wtnk", "cdrn" }
 NodAirUnitTypes = {"scrn", "scrn", "scrn"}
 AlliedUnitTypes = { "jeep", "1tnk", "ifv.ai", "1tnk", "rtnk", "ifv.ai", "ptnk", "e1", "e1", "e1", "e1", "e3", "e3", "e3", "e3" }
+GDIUnitTypes = { "hmmv", "mtnk", "vulc", "msam", "titn", "htnk.ion", "msam", "n1", "n1", "n2", "n2", "n3", "n3", "n3", "n3" }
 ScrinUnitTypes = { "seek", "seek", "gunw", "devo", "tpod", "s1", "s1", "s1", "s1", "s3", "s3" }
 ScrinAirUnitTypes = {"deva", "stmr", "stmr"}
 ProducedUnitTypes =
@@ -18,12 +19,15 @@ ProducedUnitTypes =
 	{ factory = AlliedBarracks1, types = { "e1", "e3" } },
 	{ factory = ScrinBarracks1, types = { "s1", "s3" } },
 	{ factory = NodBarracks1, types = { "n1", "n3", "n4" } },
+	{ factory = GDIBarracks1, types = { "n1", "n3", "n2" } },
 	{ factory = AlliedWarFactory1, types = { "1tnk", "2tnk", "ptnk", "jeep", "ifv.ai", "cryo", "rtnk" } },
+	{ factory = GDIWarFactory1, types = { "htnk", "msam", "titn.rail", "jugg", "htnk.ion" } },
 	{ factory = ScrinWarFactory1, types = { "seek", "tpod", "gunw", "devo", "corr" } },
 	{ factory = NodWarFactory1, types = { "ltnk", "ftnk", "stnk.nod", "arty.nod", "mlrs" } }
 }
 
 HelicopterUnitTypes = { "e1", "e1", "e1", "e1", "e3", "e3" };
+CarryAllUnitTypes = { "n1", "n1", "n1", "n2", "n3", "n3" };
 
 BindActorTriggers = function(a)
 	if a.HasProperty("Hunt") then
@@ -68,6 +72,14 @@ SendAlliedUnits = function(entryCell, unitTypes, interval)
 	Trigger.OnAllKilled(units, function() SendAlliedUnits(entryCell, unitTypes, interval) end)
 end
 
+SendGDIUnits = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(gdi, unitTypes, { entryCell }, interval)
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+	Trigger.OnAllKilled(units, function() SendGDIUnits(entryCell, unitTypes, interval) end)
+end
+
 SendScrinUnits = function(entryCell, unitTypes, interval)
 	local units = Reinforcements.Reinforce(scrin, unitTypes, { entryCell }, interval)
 	Utils.Do(units, function(unit)
@@ -106,6 +118,17 @@ InsertAlliedChinookReinforcements = function(entry, waypoint)
 	Trigger.AfterDelay(DateTime.Seconds(60), function() InsertAlliedChinookReinforcements(entry, waypoint) end)
 end
 
+InsertGDICarryAllReinforcements = function(entry, waypoint)
+	local units = Reinforcements.ReinforceWithTransport(gdi, "ocar.pod",
+		CarryAllUnitTypes, { entry.Location, waypoint.Location + CVec.New(1, 2) }, { entry.Location })[2]
+
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+
+	Trigger.AfterDelay(DateTime.Seconds(60), function() InsertGDICarryAllReinforcements(entry, waypoint) end)
+end
+
 ChronoshiftAlliedUnits = function()
 	local cells = Utils.ExpandFootprint({ AlliesOuterBase.Location }, false)
 	local units = { }
@@ -124,22 +147,37 @@ SetupFactories = function()
 	end)
 end
 
+Morning = function()
+	if (Lighting.Red < 1.0) then
+		red = red + 0.001
+	end
+
+	if (Lighting.Green < 1.0) then
+		green = green + 0.001
+	end
+	
+	if (Lighting.Blue > 1.0) then
+		blue = blue - 0.001
+	end
+	
+	if (Lighting.Ambient < 1) then
+		ambient = ambient + 0.001
+	end
+	Trigger.AfterDelay(DateTime.Seconds(10), function() Morning() end)
+end
+
 ticks = 0
 speed = 8
 
 Tick = function()
 	ticks = ticks + 1
 	
-	if (Utils.RandomInteger(1, 200) == 10) then
-		local delay = Utils.RandomInteger(1, 10)
-		Lighting.Flash("LightningStrike", delay)
-		Trigger.AfterDelay(delay, function()
-			Media.PlaySound("thunder" .. Utils.RandomInteger(1,6) .. ".aud")
-		end)
-	end
-	if (Utils.RandomInteger(1, 200) == 10) then
-		Media.PlaySound("thunder-ambient.aud")
-	end
+	Trigger.AfterDelay(DateTime.Seconds(30), Morning)
+	
+	Lighting.Red = red
+	Lighting.Green = green
+	Lighting.Blue = blue
+	Lighting.Ambient = ambient
 
 	local t = (ticks + 45) % (360 * speed) * (math.pi / 180) / speed;
 	Camera.Position = viewportOrigin + WVec.New(16200 * math.sin(t), 10480 * math.cos(t), 0)
@@ -147,9 +185,15 @@ end
 
 WorldLoaded = function()
 	allies = Player.GetPlayer("Multi0")
+	gdi = Player.GetPlayer("Multi1")
 	scrin = Player.GetPlayer("Multi2")
 	nod = Player.GetPlayer("Multi3")
 	viewportOrigin = Camera.Position
+	
+	red = 0.95
+	green = 0.85
+	blue = 1.25
+	ambient = 0.4
 
 	SetupAlliedUnits()
 	SetupFactories()
@@ -157,14 +201,15 @@ WorldLoaded = function()
 	
 	Trigger.AfterDelay(DateTime.Seconds(30), ChronoshiftAlliedUnits)
 	
-	InsertAlliedChinookReinforcements(AlliesSpawn1, DropZone1)
 	InsertAlliedChinookReinforcements(AlliesSpawn2, DropZone2)
+	InsertGDICarryAllReinforcements(AlliesSpawn1, DropZone1)
 
 	SendNodUnits(NodSpawn1.Location, NodLightUnitTypes, 50)
 	SendNodUnits(NodSpawn2.Location, NodHeavyUnitTypes, 40)
 	SendNodUnits(AlliesSpawn2.Location, NodAirUnitTypes, 300)
 	SendAlliedUnits(AlliesSpawn1.Location, AlliedUnitTypes, 50)
 	SendAlliedUnits(AlliesSpawn2.Location, AlliedUnitTypes, 40)
+	SendGDIUnits(AlliesSpawn1.Location, GDIUnitTypes, 50)
 	SendScrinUnits(ScrinSpawn1.Location, ScrinUnitTypes, 50)
 	SendScrinUnits(ScrinSpawn2.Location, ScrinUnitTypes, 40)
 	SendScrinUnits(ScrinSpawn1.Location, ScrinAirUnitTypes, 300)
