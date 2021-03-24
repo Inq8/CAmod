cdbe778689a460ebc6be8abc1c8fb51a649c1e4e
darkademic <41052878+darkademic@users.noreply.github.com>
2021-03-24 18:11:02 +0000

    Various tweaks. - Use SpeedMultiplier for aircraft warping as this doesn't cause helicopters to land (though the issue of it not affecting idling aircraft remains). - Fixed Chrono Prison hitting multiple targets that overlap. - Slight increase to Battle Tank, Heavy Tank and BTR damage vs light armor. - Reduced Desolator direct damage vs heavy armor, added a small amount of splash to main weapon, increased prone movement speed, increased irradiated unit damage and area of effect, increased deployed radiation levels at outer edges slightly. - Reduced Stealth Generator invisibility damage. - Reverted Iron Curtain footprint. - Improvements to GrantDelayedCondition for possible future usage.

--- OpenRA.Mods.CA/Traits/Conditions/GrantDelayedCondition.cs
+++ OpenRA.Mods.CA/Traits/Conditions/GrantDelayedCondition.cs
@@ -25,6 +25,12 @@ namespace OpenRA.Mods.CA.Traits
 		[Desc("Number of ticks to wait before granting the condition.")]
 		public readonly int Delay = 50;
 
+		[Desc("If the trait is disabled, revoke the condition and reset the delay.")]
+		public readonly bool RevokeOnDisabled = false;
+
+		[Desc("If the trait is paused, revoke the condition and reset the delay.")]
+		public readonly bool RevokeOnPaused = false;
+
 		public override object Create(ActorInitializer init) { return new GrantDelayedCondition(this); }
 	}
 
@@ -46,8 +52,30 @@ namespace OpenRA.Mods.CA.Traits
 			if (IsTraitPaused || IsTraitDisabled)
 				return;
 
-			if (--DelayRemaining < 1)
-				self.GrantCondition(info.Condition);
+			if (--DelayRemaining < 1 && token == Actor.InvalidConditionToken)
+				token = self.GrantCondition(info.Condition);
+		}
+
+		protected override void TraitDisabled(Actor self)
+		{
+			if (!info.RevokeOnDisabled)
+				return;
+
+			DelayRemaining = info.Delay;
+
+			if (token != Actor.InvalidConditionToken)
+				token = self.RevokeCondition(token);
+		}
+
+		protected override void TraitPaused(Actor self)
+		{
+			if (!info.RevokeOnPaused)
+				return;
+
+			DelayRemaining = info.Delay;
+
+			if (token != Actor.InvalidConditionToken)
+				token = self.RevokeCondition(token);
 		}
 	}
 }
