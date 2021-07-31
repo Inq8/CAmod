e9840312927a99f64f414c2836b81e4b6d2f79de
darkademic <41052878+darkademic@users.noreply.github.com>
2021-07-31 09:02:02 +0100

    Custom widget for lower resolution to move menu logo to right side.

---
+++ OpenRA.Mods.CA/Widgets/ImageCAWidget.cs
@@ -0,0 +1,37 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using System;
+using OpenRA.Graphics;
+using OpenRA.Mods.Common.Widgets;
+
+namespace OpenRA.Mods.CA.Widgets
+{
+	public class ImageCAWidget : ImageWidget
+	{
+		public readonly int MinXResolution = 0;
+		public readonly int MaxXResolution = 0;
+
+		public override void Draw()
+		{
+			var resolution = Game.Renderer.Resolution;
+			var resolutionWidth = resolution.Width;
+
+			if (
+				(MinXResolution > 0 && resolutionWidth < MinXResolution)
+				|| (MaxXResolution > 0 && resolutionWidth > MaxXResolution)
+			)
+				return;
+
+			base.Draw();
+		}
+	}
+}
