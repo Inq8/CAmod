f234510c57b0e91bf05ede7ac893ff2f3493afbc
darkademic <41052878+darkademic@users.noreply.github.com>
2021-11-10 23:21:11 +0000

    Updated Scrinfestation. Walls now block projectiles. Fixed dead portals continuing to spawn enemies. Added turrets. Spawn points now visible. Various other small improvements.

--- mods/ca/maps/scrinfestation/scrinfestation.lua
+++ mods/ca/maps/scrinfestation/scrinfestation.lua
@@ -2,35 +2,42 @@ Players = Player.GetPlayers(function(p) return p.Team == 1 end)
 
 AttackPaths =
 {
-	  { NW1.Location, NW2.Location, NW3.Location, NW4.Location, NW5.Location, NW6.Location, NW7.Location, NW8.Location, NW9.Location },
-      { NE1.Location, NE2.Location, NE3.Location, NE4.Location, NE5.Location, NE6.Location, NE7.Location },
-      { SE1.Location, SE2.Location, NE3.Location, NE4.Location, NE5.Location, NE6.Location, NE7.Location },
+	{
+        { NW2.Location, NW3.Location, NW4.Location, NW5.Location, NW6.Location, NW7.Location, NW8.Location, NW9.Location },
+    },
+	{
+	    { NE2.Location, NE3.Location, NE4.Location, NE5.Location, NE6.Location, NE7.Location },
+        { NE2.Location, NE3.Location, NE4.Location, NW4.Location, NW5.Location, NW6.Location, NW7.Location, NW8.Location, NW9.Location },
+	},
+	{
+        { SE2.Location, NE3.Location, NE4.Location, NE5.Location, NE6.Location, NE7.Location },
+    },
 }
 
 Wormholes = { WormholeNW, WormholeNE, WormholeSE }
 
 ScrinSquads = {
-    {"s1", "s1", "s1", "s2", "gscr"},
-    {"s1", "s1", "s1", "s3", "gscr"},
-    {"s1", "s1", "s1", "s4", "gscr"},
-    {"gscr", "gscr", "gscr"},
-    {"s4", "s4", "s4"},
-    {"s1", "s1", "s1", "s1", "s1"},
-    {"s2", "s2", "s2"},
-    {"s3", "s3", "s1", "s1"},
+	{"s1", "s1", "s1", "s2", "gscr"},
+	{"s1", "s1", "s1", "s3", "gscr"},
+	{"s1", "s1", "s1", "s4", "gscr"},
+	{"gscr", "gscr", "gscr"},
+	{"s4", "s4", "s4"},
+	{"s1", "s1", "s1", "s1", "s1"},
+	{"s2", "s2", "s2"},
+	{"s3", "s3", "s1", "s1"},
 }
 
 GetCommandosAlive = function()
-    local num = 0
+	local num = 0
 
-    for i,player in pairs(Players) do
-        local commandos = player.GetActorsByType("rmbo")
-        for j,rmbo in pairs(commandos) do
-            num = num + 1
-        end
-    end
+	for i,player in pairs(Players) do
+		local commandos = player.GetActorsByType("rmbo")
+		for j,rmbo in pairs(commandos) do
+			num = num + 1
+		end
+	end
 
-    return num
+	return num
 end
 
 IdleHunt = function(actor)
@@ -53,50 +60,55 @@ MoveAndHunt = function(actors, path)
 	end)
 end
 
-SendScrinUnits = function(entryCell, attackPath)
-    local interval = math.floor((180 / GetCommandosAlive()) + 0.5) + Utils.RandomInteger(-3,3)
-    local unitTypes = Utils.Random(ScrinSquads);
-	local units = Reinforcements.Reinforce(Scrin, unitTypes, { entryCell }, 15)
+SendScrinUnits = function(wormhole, attackPaths)
+	if not wormhole or wormhole.IsDead then
+		return
+	end
 
-    Utils.Do(units, function(unit)
-        unit.Patrol(attackPath, true, 50)
+	local interval = math.floor((150 / GetCommandosAlive()) + 0.5) + Utils.RandomInteger(-3,3)
+	local unitTypes = Utils.Random(ScrinSquads);
+	local units = Reinforcements.Reinforce(Scrin, unitTypes, { wormhole.Location }, 15)
+    local attackPath = Utils.Random(attackPaths);
 
-        Trigger.OnDamaged(unit, function()
+	Utils.Do(units, function(unit)
+		unit.Patrol(attackPath, true, 50)
 
-            Utils.Do(units, function(unit)
-                if unit.HasProperty("Hunt") and not unit.IsDead then
-                    unit.Stop()
-                    unit.Hunt()
-                    Trigger.ClearAll(unit)
-                end
-            end)
-        end)
-    end)
+		Trigger.OnDamaged(unit, function()
 
-    Trigger.AfterDelay(DateTime.Seconds(interval), function()
-        SendScrinUnits(entryCell, attackPath, interval)
-    end)
+			Utils.Do(units, function(unit)
+				if unit.HasProperty("Hunt") and not unit.IsDead then
+					unit.Stop()
+					unit.Hunt()
+					Trigger.ClearAll(unit)
+				end
+			end)
+		end)
+	end)
+
+	Trigger.AfterDelay(DateTime.Seconds(interval), function()
+		SendScrinUnits(wormhole, attackPaths, interval)
+	end)
 end
 
 WorldLoaded = function()
 	Scrin = Player.GetPlayer("Scrin")
-    local initialPlayers = GetCommandosAlive()
+	local initialPlayers = GetCommandosAlive()
 
-	SendScrinUnits(AttackPaths[1][1], AttackPaths[1])
+	SendScrinUnits(WormholeNE, AttackPaths[2])
 
-    if initialPlayers > 1 then
-        SendScrinUnits(AttackPaths[2][1], AttackPaths[2])
-    end
+	if initialPlayers > 1 then
+		SendScrinUnits(WormholeNW, AttackPaths[1])
+	end
 
-    if initialPlayers > 2 then
-        SendScrinUnits(AttackPaths[3][1], AttackPaths[3])
-    end
+	if initialPlayers > 2 then
+		SendScrinUnits(WormholeSE, AttackPaths[3])
+	end
 
-    Trigger.OnAllKilledOrCaptured(Wormholes, function()
-        local actors = Scrin.GetActors()
-        Utils.Do(actors, function(actor)
-            if actor.HasProperty("Kill") and not actor.IsDead then actor.Kill() end
-        end)
-    end)
+	Trigger.OnAllKilledOrCaptured(Wormholes, function()
+		local actors = Scrin.GetActors()
+		Utils.Do(actors, function(actor)
+			if actor.HasProperty("Kill") and not actor.IsDead then actor.Kill() end
+		end)
+	end)
 
 end
