e19663646e68cf2eae9f10b883233b2f12e62cc3
Inq <grayseer@aol.com>
2021-12-29 01:30:10 +1000

    Auto stash before merge of "master" and "darkademic/mc"

---
+++ OpenRA.Mods.CA/Traits/Conditions/ParachuteCargoOnCondition.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.CA.Activities;
+using OpenRA.Mods.Common.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	public class ParachuteCargoOnConditionInfo : ConditionalTraitInfo
+	{
+		public override object Create(ActorInitializer init) { return new ParachuteCargoOnCondition(init, this); }
+	}
+
+	public class ParachuteCargoOnCondition : ConditionalTrait<ParachuteCargoOnConditionInfo>
+	{
+		readonly ParachuteCargoOnConditionInfo info;
+
+		public ParachuteCargoOnCondition(ActorInitializer init, ParachuteCargoOnConditionInfo info)
+			: base(info)
+		{
+			this.info = info;
+		}
+
+		protected override void TraitEnabled(Actor self)
+		{
+			int unloadRange = 5;
+
+			self.CancelActivity();
+			self.QueueActivity(new ParadropCargo(self, WDist.FromCells(unloadRange), true));
+		}
+	}
+}
