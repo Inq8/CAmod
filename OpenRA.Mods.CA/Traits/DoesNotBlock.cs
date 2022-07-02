1d309103fd0b49ca560e1a23c8de1c45a63e5f3b
darkademic <41052878+darkademic@users.noreply.github.com>
2022-07-02 12:19:55 +0100

    Make cargo planes not block each other.

---
+++ OpenRA.Mods.CA/Traits/DoesNotBlock.cs
@@ -0,0 +1,45 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	[Desc("Actor with this trait can be passed through by actors with the specified target types.")]
+	public class DoesNotBlockInfo : TraitInfo
+	{
+		[Desc("Target types to allow to pass through.")]
+		public readonly BitSet<TargetableType> TargetTypes = default(BitSet<TargetableType>);
+
+		public override object Create(ActorInitializer init) { return new DoesNotBlock(init, this); }
+	}
+
+	public class DoesNotBlock : ITemporaryBlocker
+	{
+		public DoesNotBlockInfo Info { get; set; }
+
+		public DoesNotBlock(ActorInitializer init, DoesNotBlockInfo info)
+		{
+			Info = info;
+		}
+
+		bool ITemporaryBlocker.CanRemoveBlockage(Actor self, Actor blocking)
+		{
+			return Info.TargetTypes.IsEmpty || Info.TargetTypes.Overlaps(blocking.GetEnabledTargetTypes());
+		}
+
+		bool ITemporaryBlocker.IsBlocking(Actor self, CPos cell)
+		{
+			return true;
+		}
+	}
+}
