b9b989354bf686f66333988cba04171786ca7680
darkademic <41052878+darkademic@users.noreply.github.com>
2022-08-28 15:09:30 +0100

    Fixes so Temple Prime only produces Cyborgs in MQ and dogs are produced from nearest Kennel.

--- OpenRA.Mods.CA/Traits/SupportPowers/ProduceActorPowerCA.cs
+++ OpenRA.Mods.CA/Traits/SupportPowers/ProduceActorPowerCA.cs
@@ -11,10 +11,10 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using OpenRA.Graphics;
 using OpenRA.Mods.Common;
 using OpenRA.Mods.Common.Orders;
 using OpenRA.Mods.Common.Traits;
-using OpenRA.Graphics;
 using OpenRA.Primitives;
 using OpenRA.Traits;
 
