fe2e4195d86370bd01289614eee667ce7f216338
darkademic <41052878+darkademic@users.noreply.github.com>
2022-10-02 20:46:31 +0100

    Custom radar colour trait so walls always appear light grey.

---
+++ OpenRA.Mods.CA/Traits/CustomRadarColor.cs
@@ -0,0 +1,41 @@
+#region Copyright & License Information
+/*
+ * Copyright 2007-2022 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	[Desc("Define a custom radar color used regardless of owner.")]
+	public class CustomRadarColorInfo : TraitInfo
+	{
+		[Desc("Color to use")]
+		public readonly Color Color = Color.White;
+
+		public override object Create(ActorInitializer init) { return new CustomRadarColor(init, this); }
+	}
+
+	public class CustomRadarColor : IRadarColorModifier
+	{
+		public CustomRadarColorInfo Info { get; set; }
+
+		public CustomRadarColor(ActorInitializer init, CustomRadarColorInfo info)
+		{
+			Info = info;
+		}
+
+		Color IRadarColorModifier.RadarColorOverride(Actor self, Color color)
+		{
+			return Info.Color;
+		}
+	}
+}
