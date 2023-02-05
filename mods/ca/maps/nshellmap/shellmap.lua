80e26115b00fce9853fd8fc9f61a33cf1b22805c
Inq <grayseer@aol.com>
2023-02-05 12:34:14 +0800

    Update Shell Map & basic Cyborg Icons

--- mods/ca/maps/nshellmap/shellmap.lua
+++ mods/ca/maps/nshellmap/shellmap.lua
@@ -1,50 +1,56 @@
 NodLightUnitTypes = { "bike", "bike", "bggy", "n1", "n1", "n1", "n4", "n4", "n3" }
-NodHeavyUnitTypes = { "ltnk", "ltnk", "sapc.ai", "hftk", "mlrs" }
+NodHeavyUnitTypes = { "ltnk", "ltnk", "apc.ai", "hftk", "mlrs" }
 NodCyborgUnitTypes = { "n1c", "n1c", "n1c", "n1c", "n3c", "n3c", "rmbc", "rmbc" }
-AlliedUnitTypes = { "jeep", "1tnk", "apc.ai", "1tnk", "gtnk", "ifv.ai", "ptnk", "e1", "e1", "e1", "e1", "e3", "e3", "e3", "seal" }
-ScrinUnitTypes = { "seek", "atmz", "lchr", "devo", "tpod", "s1", "s1", "s1", "s1", "s3", "s3" }
-ScrinHeavyUnitTypes = { "rptp", "atmz", "ruin", "devo", "tpod", "s1", "s1", "s1", "s1", "s3", "s3" }
-ProxyType = "powerproxy.airborne"
-ParadropWaypoints = { Airdrop1, Airdrop2, Airdrop3}
-HelicopterUnitTypes = { "e1", "e1", "e1", "e1", "e3", "e3" };
+AlliedUnitTypes = { "hmmv", "mtnk", "vulc.ai", "mtnk", "msam", "vulc.ai", "htnk.ion", "n1", "n1", "n1", "n1", "n3", "n3", "n2", "rmbo" }
+SovietUnitTypes = { "3tnk.atomic", "v2rl", "isu", "4tnk.atomic", "btr.ai", "e1", "e1", "e1", "e4", "e3", "e3" }
+SovietHeavyUnitTypes = { "apoc.atomic", "3tnk.atomic", "4tnk.atomic", "btr.ai", "v2rl", "e1", "e1", "e4", "ivan", "e3", "e3" }
+ShipUnitTypes = { "3tnk", "3tnk", "btr.ai", "btr.ai", "katy" }
+ProxyType = "powerproxy.paratroopers"
+ProxySovietType = "powerproxy.shocktroopers"
+ProxyAlliesType = "powerproxy.airborne"
+ParadropWaypoints = { Airdrop2}
+SovietParadropWaypoints = { Airdrop1, Airdrop3}
+AlliesParadropWaypoints = { Airdrop4}
+Mig1Waypoints = { Mig11, Mig12, Mig13, Mig14 }
+Mig2Waypoints = { Mig21, Mig22, Mig23, Mig24 }
+HelicopterUnitTypes = { "n1", "n1", "n1", "n2", "n3", "n3" };
 ProducedUnitTypes =
 {
-	{ factory = Usainf, types = { "e1", "e3" } },
-	{ factory = Usainf2, types = { "e1", "e3" } },
-	{ factory = Scrininf, types = { "s1", "s3" } },
+	{ factory = Usainf, types = { "n1", "n3" } },
+	{ factory = Usainf2, types = { "n1", "n3" } },
+	{ factory = Ainf, types = { "e1", "e3" } },
+	{ factory = Sovietinf, types = { "e1", "e2" } },
 	{ factory = Nodinf, types = { "n1", "n3", "n4" } },
-	{ factory = Nodinf2, types = { "n1", "n3", "n4" } },
-	{ factory = Usaveh, types = { "1tnk", "2tnk", "ptnk", "jeep", "ifv.ai", "cryo", "2tnk" } },
-	{ factory = Usaveh2, types = { "1tnk", "2tnk", "ptnk", "jeep", "ifv.ai", "cryo", "2tnk" } },
-	{ factory = Scrinveh, types = { "seek", "lchr", "atmz", "devo", "corr" } },
-	{ factory = Nodveh, types = { "ltnk", "hftk", "stnk.nod", "howi" } },
-	{ factory = Nodveh2, types = { "ltnk", "hftk", "stnk.nod", "howi" } }
+	{ factory = Aveh, types = { "2tnk", "ptnk", "batf.ai" } },
+	{ factory = Usaveh, types = { "mtnk", "disr", "titn.rail", "hmmv", "htnk.ion", "msam", "mtnk" } },
+	{ factory = Usaveh2, types = { "mtnk", "disr", "titn", "hmmv", "htnk.ion", "msam", "mtnk" } },
+	{ factory = Sovietveh, types = { "3tnk.atomic", "4tnk.atomic", "katy", "v2rl", "btr.ai" } },
+	{ factory = Nodveh, types = { "ltnk", "hftk", "stnk.nod", "howi" } }
 }
 
 BindActorTriggers = function(a)
 	if a.HasProperty("Hunt") then
-		if a.Owner ~= usa then
+		if a.Owner == usa then
 			Trigger.OnIdle(a, function(a)
 				if a.IsInWorld then
-					a.Hunt()
+					a.AttackMove(NodHQ.Location)
 				end
 			end)
 		else
-			Trigger.OnIdle(a, function(a)
-				if a.IsInWorld then
-					a.Hunt()
-				end
-			end)
-		end
-	end
-
-	if a.HasProperty("HasPassengers") then
-		Trigger.OnDamaged(a, function()
-			if a.HasPassengers then
-				a.Stop()
-				a.UnloadPassengers()
+			if a.Owner == allies then
+				Trigger.OnIdle(a, function(a)
+					if a.IsInWorld then
+						a.Hunt()
+					end
+				end)
+			else
+				Trigger.OnIdle(a, function(a)
+					if a.IsInWorld then
+						a.Hunt()
+					end
+				end)
 			end
-		end)
+		end
 	end
 end
 
@@ -64,12 +70,12 @@ SendAlliedUnits = function(entryCell, unitTypes, interval)
 	Trigger.OnAllKilled(units, function() SendAlliedUnits(entryCell, unitTypes, interval) end)
 end
 
-SendScrinUnits = function(entryCell, unitTypes, interval)
-	local units = Reinforcements.Reinforce(scrin, unitTypes, { entryCell }, interval)
+SendSovietUnits = function(entryCell, unitTypes, interval)
+	local units = Reinforcements.Reinforce(Soviet, unitTypes, { entryCell }, interval)
 	Utils.Do(units, function(unit)
 		BindActorTriggers(unit)
 	end)
-	Trigger.OnAllKilled(units, function() SendScrinUnits(entryCell, unitTypes, interval) end)
+	Trigger.OnAllKilled(units, function() SendSovietUnits(entryCell, unitTypes, interval) end)
 end
 
 ProduceUnits = function(t)
@@ -80,6 +86,19 @@ ProduceUnits = function(t)
 		factory.Produce(unitType)
 		factory.CallFunc(function() ProduceUnits(t) end)
 	end
+end
+
+ParadropSovietUnits = function()
+	local lz = Utils.Random(SovietParadropWaypoints)
+	local aircraft = powersovietproxy.TargetParatroopers(lz.CenterPosition)
+
+	Utils.Do(aircraft, function(a)
+		Trigger.OnPassengerExited(a, function(t, p)
+			BindActorTriggers(p)
+		end)
+	end)
+
+	Trigger.AfterDelay(DateTime.Seconds(35), ParadropSovietUnits)
 end
 
 ParadropUSAUnits = function()
@@ -95,8 +114,44 @@ ParadropUSAUnits = function()
 	Trigger.AfterDelay(DateTime.Seconds(135), ParadropUSAUnits)
 end
 
+ParadropAlliesUnits = function()
+	local lz = Utils.Random(AlliesParadropWaypoints)
+	local aircraft = poweralliesproxy.TargetParatroopers(lz.CenterPosition)
+
+	Utils.Do(aircraft, function(a)
+		Trigger.OnPassengerExited(a, function(t, p)
+			BindActorTriggers(p)
+		end)
+	end)
+
+	Trigger.AfterDelay(DateTime.Seconds(135), ParadropUSAUnits)
+end
+
+SendMigs = function(waypoints)
+	local migEntryPath = { waypoints[1].Location, waypoints[2].Location }
+	local migs = Reinforcements.Reinforce(Soviet, { "suk" }, migEntryPath, 4)
+	Utils.Do(migs, function(mig)
+		mig.Move(waypoints[3].Location)
+		mig.Move(waypoints[4].Location)
+		mig.Destroy()
+	end)
+
+	Trigger.AfterDelay(DateTime.Seconds(40), function() SendMigs(waypoints) end)
+end
+
+ShipAlliedUnits = function()
+	local units = Reinforcements.ReinforceWithTransport(Soviet, "lst",
+		ShipUnitTypes, { LstEntry.Location, LstUnload.Location }, { LstEntry.Location })[2]
+
+	Utils.Do(units, function(unit)
+		BindActorTriggers(unit)
+	end)
+
+	Trigger.AfterDelay(DateTime.Seconds(60), ShipAlliedUnits)
+end
+
 InsertAlliedChinookReinforcements = function(entry, hpad)
-	local units = Reinforcements.ReinforceWithTransport(usa, "nhaw",
+	local units = Reinforcements.ReinforceWithTransport(usa, "tran",
 		HelicopterUnitTypes, { entry.Location, hpad.Location + CVec.New(1, 2) }, { entry.Location })[2]
 
 	Utils.Do(units, function(unit)
@@ -106,11 +161,17 @@ InsertAlliedChinookReinforcements = function(entry, hpad)
 	Trigger.AfterDelay(DateTime.Seconds(120), function() InsertAlliedChinookReinforcements(entry, hpad) end)
 end
 
+SetupFactories = function()
+	Utils.Do(ProducedUnitTypes, function(production)
+		Trigger.OnProduction(production.factory, function(_, a) BindActorTriggers(a) end)
+	end)
+end
+
 ChronoshiftAlliedUnits = function()
-	local cells = Utils.ExpandFootprint({ helispawn2.Location }, false)
+	local cells = Utils.ExpandFootprint({ ChronoshiftLocation.Location }, false)
 	local units = { }
 	for i = 1, #cells do
-		local unit = Actor.Create("gtnk", true, { Owner = usa, Facing = Angle.North })
+		local unit = Actor.Create("gtnk", true, { Owner = allies, Facing = Angle.East })
 		BindActorTriggers(unit)
 		units[unit] = cells[i]
 	end
@@ -118,47 +179,38 @@ ChronoshiftAlliedUnits = function()
 	Trigger.AfterDelay(DateTime.Seconds(60), ChronoshiftAlliedUnits)
 end
 
-SetupFactories = function()
-	Utils.Do(ProducedUnitTypes, function(production)
-		Trigger.OnProduction(production.factory, function(_, a) BindActorTriggers(a) end)
-	end)
-end
-
 ticks = 0
 speed = 30
 
 Tick = function()
 	ticks = ticks + 1
-	
-	if (Utils.RandomInteger(1, 200) == 10) then
-		local delay = Utils.RandomInteger(1, 10)
-		Lighting.Flash("LightningStrike", delay)
-		Trigger.AfterDelay(delay, function()
-			Media.PlaySound("thunder" .. Utils.RandomInteger(1,6) .. ".aud")
-		end)
-	end
-	if (Utils.RandomInteger(1, 200) == 10) then
-		Media.PlaySound("thunder-ambient.aud")
-	end
 
 	local t = (ticks + 45) % (360 * speed) * (math.pi / 180) / speed;
 	Camera.Position = viewportOrigin + WVec.New(19200 * math.sin(t), 20480 * math.cos(t), 0)
 end
 
 WorldLoaded = function()
-	usa = Player.GetPlayer("Multi0")
-	scrin = Player.GetPlayer("Multi2")
-	blackh = Player.GetPlayer("Multi3")
+	usa = Player.GetPlayer("GDI")
+	Soviet = Player.GetPlayer("Soviet")
+	blackh = Player.GetPlayer("Nod")
+	allies = Player.GetPlayer("Allies")
 	viewportOrigin = Camera.Position
 	
 	SetupFactories()
+	ShipAlliedUnits()
 	InsertAlliedChinookReinforcements(Helispawn1, Helidrop1)
-	InsertAlliedChinookReinforcements(helispawn2, Airdrop3)
+	InsertAlliedChinookReinforcements(Helispawn2, Helidrop2)
 	powerproxy = Actor.Create(ProxyType, false, { Owner = usa })
+	powersovietproxy = Actor.Create(ProxySovietType, false, { Owner = Soviet })
+	poweralliesproxy = Actor.Create(ProxyAlliesType, false, { Owner = allies })
 	ParadropUSAUnits()
-	
+	ParadropSovietUnits()
+	ParadropAlliesUnits()
 	Trigger.AfterDelay(DateTime.Seconds(5), ChronoshiftAlliedUnits)
 	Utils.Do(ProducedUnitTypes, ProduceUnits)
+	
+	Trigger.AfterDelay(DateTime.Seconds(30), function() SendMigs(Mig1Waypoints) end)
+	Trigger.AfterDelay(DateTime.Seconds(30), function() SendMigs(Mig2Waypoints) end)
 
 	SendNodUnits(Nodspawn.Location, NodLightUnitTypes, 50)
 	SendNodUnits(Nodspawn2.Location, NodHeavyUnitTypes, 40)
@@ -169,9 +221,9 @@ WorldLoaded = function()
 	SendAlliedUnits(Usaspawn.Location, AlliedUnitTypes, 50)
 	SendAlliedUnits(Helispawn1.Location, AlliedUnitTypes, 40)
 	SendAlliedUnits(Usaspawn2.Location, AlliedUnitTypes, 40)
-	SendScrinUnits(Scrinspawn.Location, ScrinUnitTypes, 50)
-	SendScrinUnits(Scrinspawn2.Location, ScrinHeavyUnitTypes, 40)
-	SendScrinUnits(wormhole1.Location, ScrinUnitTypes, 50)
-	SendScrinUnits(wormhole2.Location, ScrinUnitTypes, 50)
-	SendScrinUnits(wormhole3.Location, ScrinHeavyUnitTypes, 50)
+	SendSovietUnits(Sovietspawn.Location, SovietUnitTypes, 50)
+	SendSovietUnits(Sovietspawn2.Location, SovietHeavyUnitTypes, 40)
+	SendSovietUnits(wormhole1.Location, SovietUnitTypes, 50)
+	SendSovietUnits(wormhole2.Location, SovietUnitTypes, 50)
+	SendSovietUnits(wormhole3.Location, SovietHeavyUnitTypes, 50)
 end
