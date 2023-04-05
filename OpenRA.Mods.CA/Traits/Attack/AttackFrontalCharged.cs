4f1cc919a626b5745077ddebce2363642f60eb27
darkademic <41052878+darkademic@users.noreply.github.com>
2023-04-05 21:57:04 +0100

    Fix issue with AttackFrontalCharged being enabled while already attacking. Yaml fix.

--- OpenRA.Mods.CA/Traits/Attack/AttackFrontalCharged.cs
+++ OpenRA.Mods.CA/Traits/Attack/AttackFrontalCharged.cs
@@ -92,6 +92,12 @@ namespace OpenRA.Mods.CA.Traits
 			shotsFired = 0;
 		}
 
+		protected override void TraitEnabled(Actor self)
+		{
+			if (self.CurrentActivity is Attack && !(self.CurrentActivity is AttackCharged))
+				self.CurrentActivity.Cancel(self);
+		}
+
 		protected override void Tick(Actor self)
 		{
 			if (IsTraitDisabled || IsTraitPaused)
