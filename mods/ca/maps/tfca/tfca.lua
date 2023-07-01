9cd5d54cc77edcf2a78b7a2490356ac9a79b54fa
darkademic <41052878+darkademic@users.noreply.github.com>
2023-07-01 13:20:58 +0100

    TFCA bots.

--- mods/ca/maps/tfca/tfca.lua
+++ mods/ca/maps/tfca/tfca.lua
@@ -1,18 +1,34 @@
-UnitsPerPlayer = Map.LobbyOption("unitsperplayer")
+UnitsPerPlayer = tonumber(Map.LobbyOption("unitsperplayer"))
 
 WorldLoaded = function()
     Blue = Player.GetPlayer("Blue")
     Red = Player.GetPlayer("Red")
+    Neutral = Player.GetPlayer("Neutral")
     BlueScore = 0
     RedScore = 0
 
     Players = Player.GetPlayers(function(p) return p.Team == 1 or p.Team == 2 and not p.IsNonCombatant end)
     BluePlayers = Player.GetPlayers(function(p) return p.Team == 1 and not p.IsNonCombatant end)
     RedPlayers = Player.GetPlayers(function(p) return p.Team == 2 and not p.IsNonCombatant end)
-    Objectives = { Dome, Comms, Ref, Power, Tech }
+    BotPlayers = Player.GetPlayers(function(p) return p.IsBot end)
+    BuildableUnitTypes = { "seal", "e3", "e4", "ivan", "snip", "medi", "xo", "e6", "sab" }
+    BotInfo = { }
+    BotEngiTurrets = {
+        -- engistring = turret
+    }
+
+    Objectives = {
+        Tech = { Actor = Tech, Waypoints = { Tech1, Tech2, Tech3, Tech4 } },
+        Power = { Actor = Power, Waypoints = { Power1, Power2, Power3 } },
+        Ref = { Actor = Ref, Waypoints = { Ref1, Ref2, Ref3, Ref4, Ref5 } },
+        Comms = { Actor = Comms, Waypoints = { Comms1, Comms2, Comms3, Comms4 } },
+        Dome = { Actor = Dome, Waypoints = { Dome1, Dome2, Dome3, Dome4 } }
+    }
+
+    BotSetup()
 
     Utils.Do(Objectives, function(o)
-        Trigger.OnCapture(o, function(self, captor, oldOwner, newOwner)
+        Trigger.OnCapture(o.Actor, function(self, captor, oldOwner, newOwner)
             if newOwner.Team == 1 then
                 self.Owner = Blue
             elseif newOwner.Team == 2 then
@@ -22,7 +38,7 @@ WorldLoaded = function()
     end)
 
     Utils.Do(Players, function(p)
-        p.Cash = tonumber(UnitsPerPlayer)
+        p.Cash = UnitsPerPlayer
         local spawnId = p.Spawn
         local spawnPoint = Map.NamedActor("Spawn" .. spawnId)
 
@@ -97,6 +113,7 @@ Tick = function()
         end
 
         UpdateScoresText()
+        BotTick()
     end
 end
 
@@ -119,6 +136,10 @@ BlueWins = function()
             s.Destroy()
         end)
     end)
+    local spawnguns = Red.GetActorsByType("spawngun")
+    Utils.Do(spawnguns, function(g)
+        g.Kill()
+    end)
 end
 
 RedWins = function()
@@ -128,4 +149,156 @@ RedWins = function()
             s.Destroy()
         end)
     end)
+    local spawnguns = Blue.GetActorsByType("spawngun")
+    Utils.Do(spawnguns, function(g)
+        g.Kill()
+    end)
+end
+
+BotSetup = function()
+    Utils.Do(BotPlayers, function(p)
+        local initialObjective
+
+        if p.Team == 1 then
+            initialObjective = Objectives.Comms
+        else
+            initialObjective = Objectives.Dome
+        end
+
+        BotInfo[p.InternalName] = {
+            FirstObjective = true,
+            CurrentObjective = initialObjective
+        }
+    end)
+end
+
+BotTick = function()
+    Utils.Do(BotPlayers, function(p)
+        local botUnits = p.GetActorsByTypes(BuildableUnitTypes)
+        local botInfo = BotInfo[p.InternalName]
+
+        local alliedPlayers
+        if p.Team == 1 then
+            alliedPlayers = BluePlayers
+        else
+            alliedPlayers = RedPlayers
+        end
+
+        -- if current objective is not set, or is already owned by an ally, set a new one
+        if botInfo.CurrentObjective == nil or p.IsAlliedWith(botInfo.CurrentObjective.Actor.Owner) then
+            SelectNewObjective(p, botUnits, botInfo)
+
+        -- random chance to swap objectives every 25 seconds
+        elseif DateTime.GameTime > 1 and DateTime.GameTime % 625 == 0 and Utils.RandomInteger(0, 100) > 50 then
+            SelectNewObjective(p, botUnits, botInfo)
+        end
+
+        -- if bot has money, make a unit
+        if p.Cash > 0 then
+            local chosenType = Utils.Random(BuildableUnitTypes)
+
+            p.Build({ chosenType }, function(actors)
+                Utils.Do(actors, function(a)
+
+                    Trigger.OnIdle(a, function(self)
+
+                        if botInfo.CurrentObjective ~= nil then
+
+                            if self.Type == "medi" then
+                                local randomTarget = RandomTargetToHeal(self, alliedPlayers)
+                                if randomTarget ~= nil then
+                                    self.Guard(randomTarget)
+                                end
+                            end
+
+                            -- attack move to random location near current objective
+                            local randomWaypoint = Utils.Random(botInfo.CurrentObjective.Waypoints)
+                            self.AttackMove(randomWaypoint.Location)
+
+                            -- 75% chance to try capping if engi/xo/infil, otherwise 25%
+                            local capChance = Utils.RandomInteger(0, 100)
+                            if (self.Type == "e6" or self.Type == "sab" or self.Type == "xo" and capChance > 25) or (capChance > 75) then
+                                if self.CanCapture(botInfo.CurrentObjective.Actor) then
+                                    self.Capture(botInfo.CurrentObjective.Actor)
+                                end
+                            end
+
+                            if self.Type == "e6" then
+                                if Utils.RandomInteger(0, 100) > 66 then
+                                    local selfString = tostring(self)
+
+                                    if BotEngiTurrets[selfString] == nil or BotEngiTurrets[selfString].IsDead then
+                                        local randomTurretLocation = FindTurretLocation(self)
+
+                                        if randomTurretLocation ~= nil then
+                                            BotEngiTurrets[selfString] = Actor.Create("gun", true, { Owner = self.Owner, Location = randomTurretLocation })
+                                        end
+                                    end
+
+                                    if BotEngiTurrets[selfString] ~= nil and not BotEngiTurrets[selfString].IsDead then
+                                        local guardChance = Utils.RandomInteger(0, 100)
+                                        if guardChance > 50 then
+                                            self.Guard(BotEngiTurrets[selfString])
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end)
+                end)
+            end)
+        end
+    end)
+end
+
+RandomTargetToHeal = function(self, alliedPlayers)
+    local target = nil
+
+    Utils.Do(alliedPlayers, function(ap)
+        local units = ap.GetActorsByTypes(BuildableUnitTypes)
+        if #units > 0 then
+            possibleTargets = Utils.Where(units, function(a)
+                return a ~= self and a.Type ~= "medi" and a.Type ~= "sab"
+            end)
+            if #possibleTargets > 0 then
+                target = Utils.Random(possibleTargets)
+            end
+            return
+        end
+    end)
+
+    return target
+end
+
+FindTurretLocation = function(self)
+    local waypoints = Map.ActorsInCircle(self.CenterPosition, WDist.New(5120), function(a)
+        return a.Type == "waypoint"
+    end)
+
+    if #waypoints > 0 then
+        local randomWaypoint = Utils.Random(waypoints)
+        local footprint = Utils.ExpandFootprint({ randomWaypoint.Location }, true)
+        randomTargetCell = Utils.Random(footprint)
+        return randomTargetCell
+    end
+
+    return nil
+end
+
+SelectNewObjective = function(p, botUnits, botInfo)
+    local possibleObjectives = Utils.Where(Objectives, function(o)
+        return not p.IsAlliedWith(o.Actor.Owner)
+    end)
+
+    if #possibleObjectives > 0 then
+        local newObjective = Utils.Random(possibleObjectives)
+        botInfo.CurrentObjective = newObjective
+        botInfo.FirstObjective = false
+
+        if #botUnits > 0 then
+            Utils.Do(botUnits, function(a)
+                a.Stop()
+            end)
+        end
+    end
 end
