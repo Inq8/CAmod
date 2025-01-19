ba995181f3f7833272139c8b711eb96a9353a4cf
darkademic <41052878+darkademic@users.noreply.github.com>
2025-01-19 09:43:32 +0000

    Minimum damage for harvester are under attack notifications. Improved RA voice line.

---
+++ OpenRA.Mods.CA/Traits/Player/NotificationManager.cs
@@ -0,0 +1,42 @@
+#region Copyright & License Information
+/**
+ * Copyright (c) The OpenRA Combined Arms Developers (see CREDITS).
+ * This file is part of OpenRA Combined Arms, which is free software.
+ * It is made available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version. For more information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Tracks last notification times.")]
+	[TraitLocation(SystemActors.Player)]
+	public class NotificationManagerInfo : TraitInfo
+	{
+		public override object Create(ActorInitializer init) { return new NotificationManager(); }
+	}
+
+	public class NotificationManager
+	{
+		readonly Dictionary<string, long> lastNotificationTimes;
+
+		public NotificationManager()
+		{
+			lastNotificationTimes = new Dictionary<string, long>();
+		}
+
+		public void SetLastNotificationTime(string notificationType, long time)
+		{
+			lastNotificationTimes[notificationType] = time;
+		}
+
+		public long GetLastNotificationTime(string notificationType)
+		{
+			return lastNotificationTimes.ContainsKey(notificationType) ? lastNotificationTimes[notificationType] : 0;
+		}
+	}
+}
