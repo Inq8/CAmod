ac7cfc513a3aa994df3d96e0e4808104dcba4923
darkademic <41052878+darkademic@users.noreply.github.com>
2025-11-06 17:46:59 +0000

    Initial map conversion for co-op.

--- mods/ca/missions/coop-campaign/ca34-illumination-coop/illumination-coop.lua
+++ mods/ca/missions/coop-campaign/ca34-illumination-coop/illumination-coop.lua
@@ -1,6 +1,28 @@
 
 SetupPlayers = function()
+	Scrin = Player.GetPlayer("Scrin")
+	Nod = Player.GetPlayer("Nod")
+	TibLifeforms = Player.GetPlayer("TibLifeforms")
+	Neutral = Player.GetPlayer("Neutral")
 
+	Multi0 = Player.GetPlayer("Multi0")
+	Multi1 = Player.GetPlayer("Multi1")
+	Multi2 = Player.GetPlayer("Multi2")
+	Multi3 = Player.GetPlayer("Multi3")
+	Multi4 = Player.GetPlayer("Multi4")
+	Multi5 = Player.GetPlayer("Multi5")
+
+	MissionPlayers = Utils.Where({ Multi0, Multi1, Multi2, Multi3, Multi4, Multi5 }, function(p) return p ~= nil end)
+	MissionEnemies = { Scrin }
+
+    ORAMod = "ca"
+	coopInfo =
+	{
+		Mainplayer = Nod, -- The original single player player
+		Dummyplayer = Nod,
+		MainEnemies = MissionEnemies,
+	}
+	CoopInit25(coopInfo)
 end
 
 AfterWorldLoaded = function()
